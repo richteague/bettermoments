8fe5c0c9bb8073c08d04d7daf7ac08169148d547
richteague <richarddteague@gmail.com>
2018-09-13 12:04:10 -0400

    py2.7

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if sys.argv[-1] == "publish":
     sys.exit()
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
+with open(os.path.join(this_directory, "README.md")) as f:
     long_description = f.read()
 
 setup(
