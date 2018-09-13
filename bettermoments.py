fixing bug in uncertainty calculation

bettermoments.py
@@ -67,18 +67,19 @@ def quadratic(data, uncertainty=None, axis=0, x0=0.0, dx=1.0):
         df_max = uncertainty[get_slice(0)]**2
         df_plus = uncertainty[get_slice(1)]**2
 
-        sig2_a1 = df_max + 0.5*(df_minus + df_plus)
-        sig2_a2 = 0.5*(df_minus + df_plus)
-        sig_a1a2 = 0.5*(df_plus - df_minus)
+        sig2_a1 = df_max + 0.25*(df_minus + df_plus)
+        sig2_a2 = 0.25*(df_minus + df_plus)
+        sig_a1a2 = 0.25*(df_plus - df_minus)
 
         x_max_var = (0.5/a2)**2 * sig2_a1
         x_max_var += (0.5*a1/a2**2)**2 * sig2_a2
-        x_max_var += (0.5*a1/a2**3) * sig_a1a2
+        x_max_var -= (0.25*a1/a2**3) * sig_a1a2
 
         return x0 + dx * x_max, dx * np.sqrt(x_max_var)
 
     else:
         # The uncertainty is a scalar
-        x_max_sig = uncertainty * np.sqrt(1.0 / (8.0 * a2**2) +
-                                          3.0 * a1**2 / (2.0 * a2**4))
+        x_max_sig = uncertainty*np.sqrt((0.125*a1**2 + 0.375*a2**2)/a2**4)
+        # x_max_sig = uncertainty * np.sqrt(1.0 / (8.0 * a2**2) +
+        #                                   3.0 * a1**2 / (2.0 * a2**4))
         return x0 + dx * x_max, dx * x_max_sig