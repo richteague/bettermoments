fixed masking

bettermoments/collapse_cube.py
@@ -52,7 +52,7 @@ def collapse_quadratic(velax, data, linewidth=None, rms=None, N=5, axis=0):
                      uncertainty=np.ones(data.shape)*rms, axis=axis)
 
 
-def collapse_zeroth(velax, data, rms=None, N=5, threshold=None, mask=None,
+def collapse_zeroth(velax, data, rms=None, N=5, threshold=None, mask_path=None,
                     axis=0):
     r"""
     Collapses the cube by integrating along the spectral axis. It will return
@@ -83,9 +83,8 @@ def collapse_zeroth(velax, data, rms=None, N=5, threshold=None, mask=None,
         N (Optional[int]): Number of channels to use in the estimation of the
             noise.
         threshold (Optional[float]): Clip any pixels below this RMS value.
-        mask (Optional[ndarray]): A boolean or integeter array masking certain
-            pixels to be excluded in the fitting. Can either be a full 3D mask,
-            a 2D channel mask, or a 1D spectrum mask.
+        mask_path (Optional[str]): Path to a file containing a boolean mask,
+            either stored as `.FITS` or `.npy` file.
         axis (Optional[int]): Spectral axis to collapse the cube along.
 
     Returns:
@@ -96,8 +95,7 @@ def collapse_zeroth(velax, data, rms=None, N=5, threshold=None, mask=None,
     from bettermoments.methods import integrated_intensity
     rms, chan = _verify_data(data, velax, rms=rms, N=N, axis=axis)
     return integrated_intensity(data=data, dx=abs(chan), threshold=threshold,
-                                rms=rms, mask=_read_mask(mask, data),
-                                axis=axis)
+                                rms=rms, mask_path=mask_path, axis=axis)
 
 
 def collapse_maximum(velax, data, rms=None, N=5, axis=0):
@@ -177,7 +175,7 @@ def collapse_ninth(velax, data, rms=None, N=5, axis=0):
     M9, dM9, _ = peak_pixel(data=data, x0=velax[0], dx=chan, axis=axis)
     return M9, dM9
 
-def collapse_first(velax, data, rms=None, N=5, threshold=None, mask=None,
+def collapse_first(velax, data, rms=None, N=5, threshold=None, mask_path=None,
                    axis=0):
     r"""
     Collapses the cube using the intensity weighted average velocity (or first
@@ -208,9 +206,8 @@ def collapse_first(velax, data, rms=None, N=5, threshold=None, mask=None,
         N (Optional[int]): Number of channels to use in the estimation of the
             noise.
         threshold (Optional[float]): Clip any pixels below this RMS value.
-        mask (Optional[ndarray]): A boolean or integeter array masking certain
-            pixels to be excluded in the fitting. Can either be a full 3D mask,
-            a 2D channel mask, or a 1D spectrum mask.
+        mask_path (Optional[str]): Path to a file containing a boolean mask,
+            either stored as `.FITS` or `.npy` file.
         axis (Optional[int]): Spectral axis to collapse the cube along. By
             default this is the zeroth axis.
 
@@ -223,9 +220,9 @@ def collapse_first(velax, data, rms=None, N=5, threshold=None, mask=None,
     from bettermoments.methods import intensity_weighted_velocity as first
     rms, chan = _verify_data(data, velax, rms=rms, N=N, axis=axis)
     return first(data=data, x0=velax[0], dx=chan, rms=rms, threshold=threshold,
-                 mask=_read_mask(mask, data), axis=axis)
+                 mask_path=mask_path, axis=axis)
 
-def collapse_second(velax, data, rms=None, N=5, threshold=None, mask=None,
+def collapse_second(velax, data, rms=None, N=5, threshold=None, mask_path=None,
                     axis=0):
     r"""
     Collapses the cube using the intensity-weighted average velocity dispersion
@@ -254,9 +251,8 @@ def collapse_second(velax, data, rms=None, N=5, threshold=None, mask=None,
             calculated from the first and last N channels.
         N (Optional[int]): Number of channels to use in the estimation of the
             noise.
-        mask (Optional[ndarray]): A boolean or integeter array masking certain
-            pixels to be excluded in the fitting. Can either be a full 3D mask,
-            a 2D channel mask, or a 1D spectrum mask.
+        mask_path (Optional[str]): Path to a file containing a boolean mask,
+            either stored as `.FITS` or `.npy` file.
         threshold (Optional[float]): Clip any pixels below this RMS value.
         axis (Optional[int]): Spectral axis to collapse the cube along.
 
@@ -268,11 +264,11 @@ def collapse_second(velax, data, rms=None, N=5, threshold=None, mask=None,
     from bettermoments.methods import intensity_weighted_dispersion as second
     rms, chan = _verify_data(data, velax, rms=rms, N=N, axis=axis)
     return second(data=data, x0=velax[0], dx=chan, rms=rms,
-                  threshold=threshold, mask=_read_mask(mask, data), axis=axis)
+                  threshold=threshold, mask_path=mask_path, axis=axis)
 
 
 def collapse_width(velax, data, linewidth=0.0, rms=None, N=5, threshold=None,
-                   mask=None, axis=0):
+                   mask_path=None, axis=0):
     r"""
     Returns an effective width, a rescaled ratio of the integrated intensity
     and the line peak. For a Gaussian line profile this would be the Doppler
@@ -307,9 +303,8 @@ def collapse_width(velax, data, linewidth=0.0, rms=None, N=5, threshold=None,
         N (Optional[int]): Number of channels to use in the estimation of the
             noise.
         threshold (Optional[float]): Clip any pixels below this RMS value.
-        mask (Optional[ndarray]): A boolean or integeter array masking certain
-            pixels to be excluded in the fitting. Can either be a full 3D mask,
-            a 2D channel mask, or a 1D spectrum mask.
+        mask_path (Optional[str]): Path to a file containing a boolean mask,
+            either stored as `.FITS` or `.npy` file.
         axis (Optional[int]): Spectral axis to collapse the cube along.
 
     Returns:
@@ -321,8 +316,7 @@ def collapse_width(velax, data, linewidth=0.0, rms=None, N=5, threshold=None,
     rms, chan = _verify_data(data, velax, rms=rms, N=N, axis=axis)
     I0, dI0 = integrated_intensity(data=data, dx=abs(chan),
                                    threshold=threshold, rms=rms,
-                                   mask=_read_mask(mask, data),
-                                   axis=axis)
+                                   mask_path=mask_path, axis=axis)
     if linewidth > 0.0:
         linewidth = abs(linewidth / chan / np.sqrt(2.))
     else:
@@ -380,26 +374,6 @@ def _read_spectral_axis(header):
     return header['crval3'] + specax * header['cdelt3']
 
 
-def _estimate_RMS(data, N=5):
-    """Return the estimated RMS in the first and last N channels."""
-    x1, x2 = np.percentile(np.arange(data.shape[2]), [45, 55])
-    y1, y2 = np.percentile(np.arange(data.shape[1]), [45, 55])
-    x1, x2, y1, y2, N = int(x1), int(x2), int(y1), int(y2), int(N)
-    rms = np.nanstd([data[:N, y1:y2, x1:x2], data[-N:, y1:y2, x1:x2]])
-    return rms * np.ones(data[0].shape)
-
-
-def _read_mask(mask_path, data):
-    """Read in the mask and make sure it is the same shape as the data."""
-    if mask_path is not None:
-        mask = _get_data(mask_path)
-        if mask.shape != data.shape:
-            raise ValueError("Mismatch in mask and data shape.")
-    else:
-        mask = np.ones(data.shape)
-    return mask
-
-
 def _verify_data(data, velax, rms=None, N=5, axis=0):
     """Veryify the data shape and read in image properties."""
     if data.shape[axis] != velax.size:
@@ -408,6 +382,15 @@ def _verify_data(data, velax, rms=None, N=5, axis=0):
         rms = _estimate_RMS(data=data, N=N)
     chan = np.diff(velax).mean()
     return rms, chan
+
+
+def _estimate_RMS(data, N=5):
+    """Return the estimated RMS in the first and last N channels."""
+    x1, x2 = np.percentile(np.arange(data.shape[2]), [45, 55])
+    y1, y2 = np.percentile(np.arange(data.shape[1]), [45, 55])
+    x1, x2, y1, y2, N = int(x1), int(x2), int(y1), int(y2), int(N)
+    rms = np.nanstd([data[:N, y1:y2, x1:x2], data[-N:, y1:y2, x1:x2]])
+    return rms * np.ones(data[0].shape)
 
 
 def _collapse_beamtable(path):
@@ -508,7 +491,7 @@ def main():
                              'Same units as the brightness unit.')
     parser.add_argument('-N', default=5, type=int,
                         help='Number of end channels to use to estimate RMS.')
-    parser.add_argument('-mask', default='',
+    parser.add_argument('-mask', default=None,
                         help='Path to the mask FITS cube. Must have the same '
                              'shape as the input data.')
     parser.add_argument('-axis', default=0, type=int,
@@ -549,19 +532,19 @@ def main():
 
     if args.method == 'zeroth':
         M0, dM0 = collapse_zeroth(velax=velax, data=data, threshold=args.clip,
-                                  rms=args.rms, N=args.N, mask=args.mask,
+                                  rms=args.rms, N=args.N, mask_path=args.mask,
                                   axis=args.axis)
-        tosave['M0'], tosave['dM0'] = dM0, dM0
+        tosave['M0'], tosave['dM0'] = M0, dM0
 
     elif args.method == 'first':
         M1, dM1 = collapse_first(velax=velax, data=data, threshold=args.clip,
-                                 rms=args.rms, N=args.N, mask=args.mask,
+                                 rms=args.rms, N=args.N, mask_path=args.mask,
                                  axis=args.axis)
-        tosave['M1'], tosave['dM1'] = dM1, dM1
+        tosave['M1'], tosave['dM1'] = M1, dM1
 
     elif args.method == 'second':
         M2, dM2 = collapse_second(velax=velax, data=data, threshold=args.clip,
-                                  rms=args.rms, N=args.N, mask=args.mask,
+                                  rms=args.rms, N=args.N, mask_path=args.mask,
                                   axis=args.axis)
         tosave['M2'], tosave['dM2'] = M2, dM2
 
@@ -592,7 +575,7 @@ def main():
 
     elif args.method == 'width':
         dV, ddV = collapse_width(velax=velax, data=data, threshold=args.clip,
-                                 rms=args.rms, N=args.N, mask=args.mask,
+                                 rms=args.rms, N=args.N, mask_path=args.mask,
                                  axis=args.axis, linewidth=args.linewidth)
         tosave['dV'], tosave['ddV'] = dV, ddV
 

bettermoments/methods.py
@@ -16,36 +16,46 @@ try:
 except ImportError:
     gaussian_filter1d = None
 
+def _read_mask_path(mask_path, data):
+    """Read in the mask and make sure it is the same shape as the data."""
+    if mask_path is not None:
+        extension = mask_path.split('.')[-1].lower()
+        if extension == 'fits':
+            mask = _get_data(mask_path)
+        elif extension == 'npy':
+            mask = np.load(mask_path)
+        else:
+            raise ValueError("Mask must be a `.fits` or `.npy` file.")
+        if mask.shape != data.shape:
+            raise ValueError("Mismatch in mask and data shape.")
+    else:
+        mask = np.ones(data.shape)
+    return mask.astype('bool')
 
-def get_mask(data, rms=None, threshold=0.0, mask=None):
+def _threshold_mask(data, mask, rms=None, threshold=0.0):
     """
-    Returns a boolean mask which combines both the user provided mask and a
-    clip based on some noise level clip.
+    Combines the provided mask with a sigma mask.
 
     Args:
         data (ndarray): The data cube of intensities or flux densities.
-        rms (optional[ndarray/float]): Either an array or a single value of the
+        mask (ndarray): User provided boolean mask from ``_read_mask_path``.
+        rms (Optional[ndarray/float]): Either an array or a single value of the
             uncertainty in the image. We assume that the uncertainty is
             constant along the spectrum.
-        threshold (optional[float]): The level of sigma clipping to apply to
+        threshold (Optional[float]): The level of sigma clipping to apply to
             the data (based on the provided uncertainty).
-        mask (optional[ndarray]): User provided boolean mask.
-        axis (optional[int]): Axis along which to collapse the data.
 
     Returns:
         mask (ndarray): Boolean mask of which pixels to include.
     """
-    rms = np.zeros(data.shape) if rms is None else rms
+    if rms is None or threshold <= 0.0:
+        return mask.astype('bool')
     rms = np.atleast_1d(rms)
     if rms.ndim == 2:
         sigma_mask = abs(data) >= (threshold * rms)[None, :, :]
     else:
         sigma_mask = abs(data) >= threshold * rms
-    if mask is None:
-        mask = sigma_mask
-    else:
-        mask = np.logical_and(mask, sigma_mask)
-    return mask
+    return np.logical_and(mask, sigma_mask).astype('bool')
 
 
 def get_intensity_weights(data, mask):
@@ -66,7 +76,7 @@ def get_intensity_weights(data, mask):
     return np.where(mask, abs(data), noise)
 
 
-def integrated_intensity(data, dx=1.0, rms=None, threshold=0.0, mask=None,
+def integrated_intensity(data, dx=1.0, rms=None, threshold=0.0, mask_path=None,
                          axis=0):
     """
     Returns the integrated intensity (commonly known as the zeroth moment).
@@ -79,9 +89,9 @@ def integrated_intensity(data, dx=1.0, rms=None, threshold=0.0, mask=None,
             provided, the uncertainty on the centroid will not be estimated.
         threshold (Optional[float]): All pixel values below this value will not
             be included in the calculation of the intensity weighted average.
-        mask (Optional[ndarray]): A boolean mask (or such that it can be
-            treated as one) of pixels to include in the calculation. Must be
-            the same shape as the data array.
+        mask_path (Optional[ndarray]): A path to a boolean mask to apply to the
+            data. Must be in either ``.fits`` or ``.npy`` format and must be in
+            the same shape as ``data``.
         axis (Optional[int]): The axis along which the centroid should be
             estimated. By default this will be the zeroth axis.
 
@@ -92,20 +102,20 @@ def integrated_intensity(data, dx=1.0, rms=None, threshold=0.0, mask=None,
         dm0 (ndarray): The uncertainty on ``m0'' if an rms is given, otherwise
             None.
     """
+    mask = _read_mask_path(mask_path=mask_path, data=data)
     data = np.moveaxis(data, axis, 0)
-    mask = None if mask is None else np.moveaxis(mask, axis, 0)
-    mask = get_mask(data=data, rms=rms, threshold=threshold, mask=mask)
+    mask = np.moveaxis(mask, axis, 0)
+    mask = _threshold_mask(data=data, mask=mask, rms=rms, threshold=threshold)
     npix = np.sum(mask, axis=0)
-    npix_mask = np.where(npix > 1, 1, np.nan)
-    if mask.shape != data.shape:
-        raise ValueError("Mistmatch in data and mask shapes.")
     m0 = np.trapz(data * mask, dx=dx, axis=0)
-    dm0 = None if rms is None else dx * rms * npix**0.5 * np.ones(m0.shape)
-    return m0 * npix_mask, dm0 * npix_mask
+    if rms is None:
+        return np.where(npix > 1, m0, np.nan)
+    dm0 = dx * rms * npix**0.5 * np.ones(m0.shape)
+    return np.where(npix > 1, m0, np.nan), np.where(npix > 1, dm0, np.nan)
 
 
 def intensity_weighted_velocity(data, x0=0.0, dx=1.0, rms=None, threshold=None,
-                                mask=None, axis=0):
+                                mask_path=None, axis=0):
     """
     Returns the intensity weighted average velocity (commonly known as the
     first moment)
@@ -120,9 +130,9 @@ def intensity_weighted_velocity(data, x0=0.0, dx=1.0, rms=None, threshold=None,
             either a 2D map or a single value.
         threshold (Optional[float]): All pixel values below this value will not
             be included in the calculation of the intensity weighted average.
-        mask (Optional[ndarray]): A boolean mask (or such that it can be
-            treated as one) of pixels to include in the calculation. Must be
-            the same shape as the data array.
+        mask_path (Optional[ndarray]): A path to a boolean mask to apply to the
+            data. Must be in either ``.fits`` or ``.npy`` format and must be in
+            the same shape as ``data``.
         axis (Optional[int]): The axis along which the centroid should be
             estimated. By default this will be the zeroth axis.
 
@@ -132,47 +142,44 @@ def intensity_weighted_velocity(data, x0=0.0, dx=1.0, rms=None, threshold=None,
         x_max_sig (ndarray): The uncertainty on ``x_max'' if an uncertainty is
             given, otherwise None.
     """
+    mask = _read_mask_path(mask_path=mask_path, data=data)
     data = np.moveaxis(data, axis, 0)
-    mask = None if mask is None else np.moveaxis(mask, axis, 0)
-    mask = get_mask(data=data, rms=rms, threshold=threshold, mask=mask)
-    if mask.shape != data.shape:
-        raise ValueError("Mistmatch in data and mask shapes.")
-    weights = get_intensity_weights(data, mask)
+    mask = np.moveaxis(mask, axis, 0)
+    mask = _threshold_mask(data=data, mask=mask, rms=rms, threshold=threshold)
     npix = np.sum(mask, axis=0)
-    npix_mask = np.where(npix > 1, 1, np.nan)
+    weights = get_intensity_weights(data, mask)
     vpix = dx * np.arange(data.shape[0]) + x0
     vpix = vpix[:, None, None] * np.ones(data.shape)
 
     # Intensity weighted velocity.
     m1 = np.average(vpix, weights=weights, axis=0)
     if rms is None:
-        return m1 * npix_mask, None
+        return np.where(npix > 1, m1, np.nan), None
 
     # Calculate uncertainty if rms provided.
     dm1 = (vpix - m1[None, :, :]) * rms / np.sum(weights, axis=0)
     dm1 = np.sqrt(np.sum(dm1**2, axis=0))
-    return m1 * npix_mask, dm1 * npix_mask
+    return np.where(npix > 1, m1, np.nan), np.where(npix > 1, dm1, np.nan)
 
 
 def intensity_weighted_dispersion(data, x0=0.0, dx=1.0, rms=None,
-                                  threshold=None, mask=None, axis=0):
+                                  threshold=None, mask_path=None, axis=0):
     """
     Returns the intensity weighted velocity dispersion (second moment).
     """
 
     # Calculate the intensity weighted velocity first.
     m1 = intensity_weighted_velocity(data=data, x0=x0, dx=dx, rms=rms,
-                                     threshold=threshold, mask=mask,
+                                     threshold=threshold, mask_path=mask_path,
                                      axis=axis)[0]
 
     # Rearrange the data to what we need.
+    mask = _read_mask_path(mask_path=mask_path, data=data)
     data = np.moveaxis(data, axis, 0)
-    mask = None if mask is None else np.moveaxis(mask, axis, 0)
-    mask = get_mask(data=data, rms=rms, threshold=threshold, mask=mask)
-    if mask.shape != data.shape:
-        raise ValueError("Mistmatch in data and mask shapes.")
-    weights = get_intensity_weights(data, mask)
+    mask = np.moveaxis(mask, axis, 0)
+    mask = _threshold_mask(data=data, mask=mask, rms=rms, threshold=threshold)
     npix = np.sum(mask, axis=0)
+    weights = get_intensity_weights(data, mask)
     npix_mask = np.where(npix > 1, 1, np.nan)
     vpix = dx * np.arange(data.shape[0]) + x0
     vpix = vpix[:, None, None] * np.ones(data.shape)