bf555c05954053d110f1853c835f6ebd0c382dde
Richard Teague <richard.d.teague@cfa.harvard.edu>
2022-03-22 21:31:03 -0400

    added logos

--- docs/conf.py
+++ docs/conf.py
@@ -64,3 +64,6 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_favicon = "_static/bettermoments_favicon.png"
+html_logo = "_static/bettermoments_logo.png"
+html_theme_options = {"logo_only": True}
